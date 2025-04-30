aba86f0e6a447b3680790b0a2462e9e584bc4d33
dyeibs <johnverelideros@gmail.com>
2025-04-30 18:53:50 +0800

    Create register.php

---
+++ register.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<body>
+    <header>
+        <div class="logo">
+            <img src="SPCF Logo.png" alt="SPCF Logo"> 
+            <h1>iFound.</h1>
+        </div>
+        <div class="menu-icon">☰</div>
+    </header>
+    <div class="main-content">
+        <div class="left-side">
+            <h1>Lost it?<br><span>iFound it!</span></h1>
+            <p>SPCF Exclusive Lost and Found Management System.<br> We've got you.</p>
+        </div>
+<body>
+    <div class="container">
+        <div class="login-box">
+            <h2>REGISTER</h2>
+            <form action="signup.php" method="POST">
+                
+                <div class="input-box">
+                <i class="fa-solid fa-user"></i>
+                <input type="text" name="student_id" placeholder="ID" required>
+            </div>
+            <div class="input-box">
+                <i class="fa-solid fa-lock"></i>
+                <input type="password" name="password" placeholder="Password" required>
+            </div>
+                <button type="submit">Sign Up</button>
+                <a href="login.php" class="btn-signup">Already have an account? Login</a>
+            </form>
+            
+        </div>
+    </div>
+    
+</body>
+</html>
